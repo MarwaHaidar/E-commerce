ecf09ed7315aced85881050264e956b4f802cde9
wassimyounes <wass.younes@gmail.com>
2024-01-17 19:34:27 +0200

    add image upload to products tested ok

--- server/controllers/productcontroller.js
+++ server/controllers/productcontroller.js
@@ -1,23 +1,28 @@
 import Product from "../models/product.js";
 import slugify from 'slugify';
 import asyncHandler from 'express-async-handler';
+import { uploadImage, uploadMultipleImages } from "./imageuploadcontroller.js"
 
 
+
+// Call your function to handle multiple image uploads
 
 // create product 
-const createProduct = asyncHandler(async(req,res)=>{
-const name = req.body.name;
-const desc = req.body.desc;
-const price = req.body.price;
-const priceAfterDiscount = req.body.priceAfterDiscount;
-const currency = req.body.currency;// Assuming currency is provided as an ObjectId 
-const subcategory = req.body.subcategory;// Assuming subcategory is provided as an ObjectId
-const variations = req.body.variations;
-// const imageCover = req.body.imageCover;
-// const images = req.body.images;
+const createProduct = asyncHandler(async (req, res) => {
+  const name = req.body.name;
+  const desc = req.body.desc;
+  const price = req.body.price;
+  const priceAfterDiscount = req.body.priceAfterDiscount;
+  const currency = req.body.currency;// Assuming currency is provided as an ObjectId 
+  const subcategory = req.body.subcategory;// Assuming subcategory is provided as an ObjectId
+  const variations = req.body.variations;
+  const multiimages = req.files ? req.files.map(file => file.buffer) : [];
+  const imagesArray = await uploadMultipleImages(multiimages);
+  const imageCover = imagesArray[0];
+  const images = imagesArray.slice(1);
 
-const product = await Product.create({name,slug:slugify(name),desc,price,priceAfterDiscount,currency,variations,subcategory});
-res.status(201).json({data:product});
+  const product = await Product.create({ name, slug: slugify(name), desc, price, priceAfterDiscount, currency, variations, subcategory, images, imageCover });
+  res.status(201).json({ data: product });
 
 });
 export { createProduct };
@@ -25,12 +30,12 @@ export { createProduct };
 
 
 // get all products
-const getproducts = asyncHandler(async(req,res)=>{
-const page=req.query.page*1 || 1;// req.query: take data from url not from req body, *1 to change it from string to number
-const limit=req.query.limit*1 || 5; // in selected page give 5 categories
-const skip=(page-1)*limit
-const products= await Product.find({}).skip(skip).limit(limit);
-res.status(200).json({result:products.length,page,data:products});
+const getproducts = asyncHandler(async (req, res) => {
+  const page = req.query.page * 1 || 1;// req.query: take data from url not from req body, *1 to change it from string to number
+  const limit = req.query.limit * 1 || 5; // in selected page give 5 categories
+  const skip = (page - 1) * limit
+  const products = await Product.find({}).skip(skip).limit(limit);
+  res.status(200).json({ result: products.length, page, data: products });
 
 });
 
@@ -40,57 +45,61 @@ export { getproducts };
 
 // get specific product
 
-const getproduct = asyncHandler(async(req,res)=>{
-const { id } = req.params;
-const product = await Product.findById(id);
-if(!product){
- res.status(404).json({msg:`no product for this id ${id}`})
-}
-res.status(200).json({data:product})
+const getproduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  if (!product) {
+    res.status(404).json({ msg: `no product for this id ${id}` })
+  }
+  res.status(200).json({ data: product })
 });
 export { getproduct };
 
 
 // update specific product 
 
-const updateproduct = asyncHandler(async(req,res)=>{
-const { id } = req.params;
-const {name}=req.body;
-const {desc}=req.body;
-const {price}= req.body;
-const {priceAfterDiscount} = req.body;
-const {currency} = req.body;
-const {variations} = req.body;
-const {subcategory}=req.body;
+const updateproduct = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const { desc } = req.body;
+  const { price } = req.body;
+  const { priceAfterDiscount } = req.body;
+  const { currency } = req.body;
+  const { variations } = req.body;
+  const { subcategory } = req.body;
+  const multiimages = req.files ? req.files.map(file => file.buffer) : [];
+  const imagesArray = await uploadMultipleImages(multiimages);
+  const imageCover = imagesArray[0];
+  const images = imagesArray.slice(1);
 
-// Check if 'name' is provided in the request body before generating the slug
+  // Check if 'name' is provided in the request body before generating the slug
   const slug = name ? slugify(name, { lower: true }) : undefined;
 
   const product = await Product.findOneAndUpdate(
     { _id: id },
-    { name, slug, desc, price,priceAfterDiscount,currency,variations,subcategory },
+    { name, slug, desc, price, priceAfterDiscount, currency, variations, subcategory, imageCover, images },
     { new: true }
   );
 
-    if(!product){
-        res.status(404).json({msg:`no product for this is ${id}`})
-    }
-    res.status(200).json({data:product})
+  if (!product) {
+    res.status(404).json({ msg: `no product for this is ${id}` })
+  }
+  res.status(200).json({ data: product })
 })
 export { updateproduct };
 
 
 // delete specific product 
 
-const deleteproduct = asyncHandler(async(req,res) =>{
+const deleteproduct = asyncHandler(async (req, res) => {
 
-    const {id} = req.params;
-    const product = await Product.findOneAndDelete({_id:id});
-    if(!product){
-        res.status(404).json({msg:`NO Product FOR THIS ID ${id}`});
+  const { id } = req.params;
+  const product = await Product.findOneAndDelete({ _id: id });
+  if (!product) {
+    res.status(404).json({ msg: `NO Product FOR THIS ID ${id}` });
 
-    }
-    res.status(200).json({msg: `the category  was deleted successfully`})
-}) 
-export {deleteproduct}
+  }
+  res.status(200).json({ msg: `the category  was deleted successfully` })
+})
+export { deleteproduct }
 
--- server/routes/productRoute.js
+++ server/routes/productRoute.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import {createProduct ,getproducts,getproduct,updateproduct,deleteproduct} from '../controllers/productcontroller.js'
+import upload from '../controllers/imageuploadcontroller.js';
+import { createProduct, getproducts, getproduct, updateproduct, deleteproduct } from '../controllers/productcontroller.js'
 
 
 const router = express.Router();
 
-router.post('/admin/product',createProduct);
-router.get('/products',getproducts);
-router.get('/products/:id',getproduct);
-router.put('/admin/products/:id',updateproduct);
-router.delete('/products/:id',deleteproduct);
+router.post('/admin/product', upload.array("images"),  createProduct);
+router.get('/products', getproducts);
+router.get('/products/:id', getproduct);
+router.put('/admin/products/:id', upload.array("images"), updateproduct);
+router.delete('/products/:id', deleteproduct);
 
 
 export default router;
