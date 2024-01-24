a2f568c35dcb36f44fcd8284148ea68dd2ec9f6c
oussama hamzeh <ohamzeh10@gmail.com>
2024-01-24 17:20:21 +0200

    update quantity by oussama

--- server/routes/orderRoute.js
+++ server/routes/orderRoute.js
@@ -4,7 +4,21 @@ import {calculateQuantity} from '../Middleware/RecalculateQu.js'
 
 const router = express.Router();
 
-router.post('/user/order',createOrder,calculateQuantity);
+router.post('/user/order', async (req, res, next) => {
+    try {
+         // Call createOrder first
+        await createOrder(req, res, next);
+
+        // Then call calculateQuantity
+        await calculateQuantity(req, res, next);
+    } catch (error) {
+        console.error('Error processing order:', error.message);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+
+// router.post('/user/order',createOrder,calculateQuantity,);
 router.get('/orders',getorders);
 router.get('/orders/:id',getorder)
 router.put('/admin/orders/:id',updateorder)
