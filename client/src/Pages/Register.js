46ed9881e1f1737b892fb17091d28e133d0f39c6
MarwaHaidar <131484013+MarwaHaidar@users.noreply.github.com>
2024-01-31 02:51:48 +0200

    footer

---
+++ client/src/Pages/Register.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Register = () => {
+  return (
+    <div>
+         <div className="text-3xl font-bold underline test" >Register</div>
+      
+    </div>
+  )
+}
+
+export default Register
