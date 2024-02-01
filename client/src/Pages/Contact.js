a1f26eb7eff45d3315941c9d0852056ecb4bc88e
oussama hamzeh <ohamzeh10@gmail.com>
2024-02-02 01:12:05 +0200

    create contact us with oussama

--- client/src/Pages/Contact.js
+++ client/src/Pages/Contact.js
@@ -1,8 +1,18 @@
-import React from 'react'
+import React from "react";
+import InformationComponent from "../Components/ContactUs/Information/InformationComponent";
+import SendInformationComponent from "../Components/ContactUs/sendInformationComponent.js/sendInformationComponent";
 
 const Contact = () => {
+  const style = {
+    display: "flex",
+    margin: "0 auto",
+  };
+
   return (
-    <div className="text-3xl font-bold underline test" >Contact page</div>
-  )
-}
+    <div style={style}>
+      <InformationComponent />
+      <SendInformationComponent />
+    </div>
+  );
+};
 export default Contact;
