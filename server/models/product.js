f39aec37404a8eae97e5ca11111078975a0524ee
oussama hamzeh <ohamzeh10@gmail.com>
2024-02-06 10:18:40 +0200

    new add product by oussama

--- server/models/product.js
+++ server/models/product.js
@@ -1,31 +1,31 @@
-import { Schema, model } from 'mongoose';
-
+import { Schema, model } from "mongoose";
 
 // Subdocument schema for variations
 const variationSchema = new Schema({
-
   colors: [
     {
       color: { type: String, required: true },
-      sizes:[{
-        enum: ['small', 'medium', 'large'],
-        quantitySizes: { type: Number, required: true, min: 0 },
-      }],
+      sizes: [
+        {
+          enum: ["small", "medium", "large"],
+          quantitySizes: { type: Number, required: true, min: 0 },
+        },
+      ],
       quantity: { type: Number, required: true, min: 0 },
-    }],
+    },
+  ],
 });
 
-
 // {
-  // "colors": [
-  //   {
-  //     "color": "Red",
-  //     "quantity": 10,
-  //     "sizes": [
-  //       { "size": "small", "quantitySizes": 3 },
-  //       { "size": "medium", "quantitySizes": 5 },
-  //       { "size": "large", "quantitySizes": 2 }
-  //     ]
+//   "colors": [
+//     {
+//       "color": "Red",
+//       "quantity": 10,
+//       "sizes": [
+//         { "size": "small", "quantitySizes": 3 },
+//         { "size": "medium", "quantitySizes": 5 },
+//         { "size": "large", "quantitySizes": 2 }
+//       ]
 //     },
 //     {
 //       "color": "Blue",
@@ -39,66 +39,54 @@ const variationSchema = new Schema({
 //   ]
 // }
 
-
-
 // Main product schema
-const productSchema = new Schema({
+const productSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
 
-  name: {
-    type: String,
-    required: true
+    slug: {
+      type: String,
+      lowecase: true,
+    },
+
+    desc: {
+      type: String,
+      required: true,
+    },
+
+    price: { type: Number, required: true, min: 0 },
+
+    priceAfterDiscount: {
+      type: Number,
+    },
+    imageCover: {
+      type: String,
+    },
+    images: [String],
+    currency: {
+      type: Schema.Types.ObjectId,
+      ref: "Currency",
+    },
+    variations: [variationSchema],
+    subcategory: {
+      type: Schema.Types.ObjectId,
+      ref: "Subcategory",
+      required: true,
+    },
+    isFeatured: {
+      type: Boolean, // to get the product who need to review in home page
+      default: false,
+    },
+    totalQuantityProducts: {
+      type: Number,
+      default: 0,
+    },
   },
-
-  slug: {
-    type: String,
-    lowecase: true,
-  },
-
-  desc: {
-    type: String,
-    required: true
-  },
-
-  price: { type: Number,
-    required: true,
-     min: 0 },
-
-  priceAfterDiscount: {
-    type: Number
-  },
-  imageCover: {
-    type: String,
-
-  }
-  ,
-  images: [String]
-  ,
-
-  currency: {
-    type: Schema.Types.ObjectId,
-    ref: 'Currency'
-    
-  }
- ,
-  variations: [variationSchema]
-  ,
-  subcategory: {
-    type: Schema.Types.ObjectId,
-    ref: 'Subcategory',
-    required: true
-  }
-  ,
-  isFeatured: {
-    type: Boolean, // to get the product who need to review in home page 
-    default: false
-  },
-  totalQuantityProducts:{
-    type: Number,
-    default:0
-  }
-  
-}, { timestamps: true });
-
+  { timestamps: true }
+);
 
 // // Method to calculate total quantity
 // variationSchema.methods.calculateTotalQuantity = function () {
@@ -115,9 +103,5 @@ const productSchema = new Schema({
 //   return totalQuantity;
 // };
 
-
-
-
-
-const Product = model('Product', productSchema);
+const Product = model("Product", productSchema);
 export default Product;
