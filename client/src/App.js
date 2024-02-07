eb229d496c3fd00bb93ba2899391107cfef9e113
MarwaHaidar <131484013+MarwaHaidar@users.noreply.github.com>
2024-02-07 11:47:38 +0200

    registerverify

--- client/src/App.js
+++ client/src/App.js
@@ -5,12 +5,11 @@ import About from "./Pages/About.js";
 import Contact from "./Pages/Contact.js";
 import Login from "./Pages/Login.js";
 import Cart from "./Pages/Cart.js";
-import VerificationComponent from './Components/loginSign/emailverification';
+import Registerverify from "./Pages/Registerverify.js"
 import Wishlist from "./Pages/Wishlist.js";
 import Header from "./Components/Header/Header.js";
 import Footer from "./Components/Footer/Footer.js";
 import Signup from "./Pages/Signup.js";
-
 import ForgetPass from "./Pages/ForgetPass.js";
 import ProductDetails from "./Pages/ProductDetails.js";
 import NotFound from "./Pages/NotFound.js";
@@ -24,7 +23,6 @@ export default function App() {
     <div>
       <Router>
         <Header />
-
         <Routes>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
@@ -43,13 +41,15 @@ export default function App() {
           />
           <Route path="/admin" element={<Admin />} />
           <Route path="*" element={<NotFound />} />
-        </Routes>
-        <Routes>
-          <Route path="/registerverify/:token" element={<VerificationComponent />} />
-          <Route path="/register" element={<Signup />} /></Routes>
+          {/* Place the VerificationComponent route inside the Routes */}
+          <Route path="/registerverify" element={<Registerverify />} />
 
+
+          <Route path="/register" element={<Signup />} />
+        </Routes>
         <Footer />
       </Router>
     </div>
   );
 }
+
