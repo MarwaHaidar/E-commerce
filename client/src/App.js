a12ae2ecb83539e1c0b7915d5f3890cb7e5194b5
Rayan soltan <rayansoltan99@gmail.com>
2024-02-11 22:20:56 +0200

    update

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import NotFound from "./Pages/NotFound.js";
 import AllCategories from "./Pages/AllCategories.js";
 import SubCategories from "./Pages/SubCategories.js";
 import Admin from "./Pages/Admin.js";
-import ProductsView from "./Components/Home/BrowseProducts/ProductsView.js";
+import ProductsView from "./Components/Home/browseProducts/ProductsView.js";
 import AdminHeader from "./Components/Admin/AdminHeader/AdminHeader.js";
 import AdminCharts from "./Pages/AdminCharts.js";
 import CategoryAdminEdit from "./Components/Admin/CategoryAdmin/CategoryAdminEdit.js";
