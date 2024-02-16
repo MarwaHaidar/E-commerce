af560fc8e1ac7bfa381a185e171ab8c0700607aa
wassimyounes <wass.younes@gmail.com>
2024-02-16 20:18:34 +0200

    before pull

--- client/src/Components/Home/Browsecategory/browsecategory.js
+++ client/src/Components/Home/Browsecategory/browsecategory.js
@@ -37,14 +37,14 @@ const Browsecategory = ({ categories }) => {
           <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8 mt-[-50px] ml-[-20px] relative" >
 
             <div className="absolute left-0 top-1/2 z-10 transform -translate-y-1/2 cursor-pointer" >
-              <div className="rounded-full bg-gray-200 p-4 hover:bg-gray-300 transition-colors duration-300" onClick={prevSlide}>
+              <div className="rounded-full ml-9 bg-gray-200 p-4 hover:bg-gray-300 transition-colors duration-300" onClick={prevSlide}>
                 <FaChevronLeft className="text-gray-700 h-6 w-6" />
               </div>
             </div>
             {categories.slice(startIndex, startIndex + 4).map((category) => (
               <Categoriescard key={category.id} category={category} />
             ))}
-     <div className="absolute right-0 top-1/2 z-10 transform -translate-y-1/2 cursor-pointer" >
+            <div className="absolute right-0 top-1/2 z-10 transform -translate-y-1/2 cursor-pointer" >
               <div className="rounded-full bg-gray-200 p-4 hover:bg-gray-300 transition-colors duration-300" onClick={nextSlide}>
                 <FaChevronRight className="text-gray-700 h-6 w-6" />
               </div>
@@ -53,8 +53,8 @@ const Browsecategory = ({ categories }) => {
         </div>
       </div>
       <Link to="/categories">
-      <Viewproductbtn text="View All Categories" />
-    </Link>
+        <Viewproductbtn text="View All Categories" />
+      </Link>
     </>
   );
 };
